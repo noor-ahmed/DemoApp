89dc1e23cf67454bb3ecf8569dbff30ba55dd115
Noor Ahmed Biswas <noor.ahmed.biswas@framgia.com>
2015-10-24 18:51:12 +0000

    70%

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
  
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
   get  'static_pages/home'
   get	'signup'		=>	'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  
+
   resources	:users
+  resources :microposts, only: [:create, :destroy]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
